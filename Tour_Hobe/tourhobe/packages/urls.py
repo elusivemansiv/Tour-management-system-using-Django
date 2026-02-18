9e6a0389028fe85eded5176f82a89a387bb19d91
elusivemansiv <mansibahsan9@gmail.com>
2026-02-18 11:56:48 +0600

    template updated

--- Tour_Hobe/tourhobe/packages/urls.py
+++ Tour_Hobe/tourhobe/packages/urls.py
@@ -3,5 +3,6 @@ from .views import home, location_packages
 
 urlpatterns = [
     path('', home, name='home'),
+    
     path('location/<int:location_id>/', location_packages, name='location_packages'),
 ]
